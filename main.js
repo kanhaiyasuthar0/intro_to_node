69780d70e7b7605aad11223e4294a8274fa167ea
Kanhaiya Suthar <kanhaiyasuthar0@gmail.com>
2022-04-15 19:09:51 +0530

    intro to node

---
+++ main.js
@@ -0,0 +1,8 @@
+const add = require("./add.js");
+const substract = require("./substract.js");
+const multiply = require("./multiply.js");
+const divide = require("./divide.js");
+console.log(add(2,4))
+console.log(substract(2,4))
+console.log(multiply(2,4))
+console.log(divide(2,4))
